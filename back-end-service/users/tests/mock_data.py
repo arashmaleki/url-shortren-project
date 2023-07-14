6ce0332606de5ad1392a4b6250fc02244b4ca229
arash maleki <arashmaleki77@gmail.com>
2023-07-15 01:56:33 +0330

    add user test case

---
+++ back-end-service/users/tests/mock_data.py
@@ -0,0 +1,10 @@
+user_register_data = {
+    "username": "test1234",
+    "email": "testuser@example.com",
+    "password": "testpassword",
+    "first_name": "Test",
+    "last_name": "User",
+}
+
+user_login_data = {"username": "test1234", "password": "testpassword"}
+user_invalid_login_data = {"username": "test1234", "password": "wrongpassword"}
