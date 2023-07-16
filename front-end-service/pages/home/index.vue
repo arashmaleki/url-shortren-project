7d88169f21b0316b09320894902ca8b390c59af5
arash maleki <arashmaleki77@gmail.com>
2023-07-16 03:49:01 +0330

    complete home page

--- front-end-service/pages/home/index.vue
+++ front-end-service/pages/home/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-
+    <shortener-url />
   </div>
 </template>
 
@@ -8,31 +8,6 @@
   export default {
     name: 'index',
     layout: 'default',
-    data() {
-      return {
-        shortener_urls: [],
-      }
-    },
-    mounted() {
-      if (this.$auth.loggedIn) {
-        this.get_user_urls()
-
-      } else {
-        this.$router.push({
-          path: '/home'
-        })
-      }
-    },
-    methods: {
-      async get_user_urls() {
-        try {
-          let response = await this.$axios.$get('/shortener')
-          this.shortener_urls = response.results
-        } catch (error) {
-
-        }
-      }
-    },
   }
 </script>
 
