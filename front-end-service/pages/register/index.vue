9a9049024413426d0f0a219bab4a6099162f46d9
arash maleki <arashmaleki77@gmail.com>
2023-07-16 03:49:18 +0330

    complete register page

--- front-end-service/pages/register/index.vue
+++ front-end-service/pages/register/index.vue
@@ -1,32 +1,43 @@
 <template>
-  <div>
-    <b-form @submit="register">
-      <b-form-group id="email" label="Email Address:" label-for="input-email">
-        <b-form-input id="input-email" v-model="user_data.email" type="email" placeholder="Enter email" required>
-        </b-form-input>
-      </b-form-group>
+  <div class="centerForm">
+    <b-card class="fixedForm mt-5" title="Register">
+      <b-form @submit.prevent="register()" class="mt-4">
+        <div class="d-flex">
+          <b-form-group id="first-name" class="mr-3" label="First Name:" label-for="input-first-name">
+            <b-form-input id="input-first-name" v-model="user_data.first_name" placeholder="First Name" required>
+            </b-form-input>
+          </b-form-group>
 
-      <b-form-group id="first-name" label="First Name:" label-for="input-first-name">
-        <b-form-input id="input-first-name" v-model="user_data.first_name" placeholder="First Name" required>
-        </b-form-input>
-      </b-form-group>
+          <b-form-group id="last-name" label="Last Name:" label-for="input-last-name">
+            <b-form-input id="input-last-name" v-model="user_data.last_name" placeholder="Last Name" required>
+            </b-form-input>
+          </b-form-group>
+        </div>
 
-      <b-form-group id="last-name" label="Last Name:" label-for="input-last-name">
-        <b-form-input id="input-last-name" v-model="user_data.last_name" placeholder="Last Name" required>
-        </b-form-input>
-      </b-form-group>
+        <b-form-group id="input-username" label="Username:" label-for="input-username">
+          <b-form-input id="input-username" v-model="user_data.username" placeholder="Username" required></b-form-input>
+        </b-form-group>
 
-      <b-form-group id="input-username" label="Username:" label-for="input-username">
-        <b-form-input id="input-username" v-model="user_data.username" placeholder="Username" required></b-form-input>
-      </b-form-group>
+        <b-form-group id="email" label="Email Address:" label-for="input-email">
+          <b-form-input id="input-email" v-model="user_data.email" type="email" placeholder="Email" required>
+          </b-form-input>
+        </b-form-group>
 
-      <b-form-group id="input-password" label="Password:" label-for="input-password">
-        <b-form-input id="input-password" v-model="user_data.password" type="password" placeholder="Password" required>
-        </b-form-input>
-      </b-form-group>
+        <b-form-group id="input-password" label="Password:" label-for="input-password">
+          <b-form-input id="input-password" v-model="user_data.password" type="password" placeholder="Password"
+            required>
+          </b-form-input>
+        </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-    </b-form>
+        <b-overlay :show="is_loading">
+          <b-button class="mt-3 mb-3 w-100" type="submit" variant="primary">Register</b-button>
+        </b-overlay>
+        <div class="errorMessage textAlignCenter">
+          {{ error }}
+        </div>
+        
+      </b-form>
+    </b-card>
   </div>
 </template>
 
@@ -44,19 +55,19 @@
           last_name: ''
         },
         error: '',
-        is_loading: false
+        is_loading: false,
       }
     },
     methods: {
       async register() {
-        this.is_loading = true
         try {
-          let response = await this.$axios.$post('/users/register', this.user_data)
+          this.is_loading = true
+          let response = await this.$axios.$post('/api/user/register/', this.user_data)
           this.$router.push({
             path: '/login'
           })
         } catch (error) {
-          this.error = error
+          this.error = error.response.data
         }
         this.is_loading = false
       },
