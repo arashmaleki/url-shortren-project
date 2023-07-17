54b38eda84512b5b0f56e5a36c8117da29cb085d
arash maleki <arashmaleki77@gmail.com>
2023-07-17 20:28:48 +0330

    add shortener test case

---
+++ back-end-service/shortener/tests/mock_data.py
@@ -0,0 +1,23 @@
+shortener_data = {
+    "long_url": "Bitcoin",
+}
+
+shortener2_data = {
+    "long_url": "Bitcoin",
+}
+
+create_shortener_data = {
+    "long_url": "https://aws.amazon.com/premiumsupport/?nc2=h_ql_ce_spt-a"
+}
+
+update_shortener_data = {
+    "long_url": "https://stackoverflow.com/"
+}
+
+admin_data = {
+    'email': "admin@example.com",
+    'username': 'adminusername',
+    'password': 'password123',
+    'first_name': 'adminfirstname',
+    'last_name': 'adminlastname'
+}
